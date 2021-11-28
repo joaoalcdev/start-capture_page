3ed2d9f039e71c08d1d72737e107b4c2df65d1e0
João Alcântara <joaosantalcantara@gmail.com>
2021-11-28 07:39:46 -0300

    fix week clock

--- src/assets/components/HomeSection/HomeSection.tsx
+++ src/assets/components/HomeSection/HomeSection.tsx
@@ -17,7 +17,7 @@ const HomeSection = () => {
 
   const startTimer = () => {
     const countDownDate = new Date(
-      'Mon Nov 29 2021 18:00:00 GMT-0200'
+      'Fri Dec 10 2021 18:00:00 GMT-0200'
     ).getTime()
 
     interval = setInterval(() => {
